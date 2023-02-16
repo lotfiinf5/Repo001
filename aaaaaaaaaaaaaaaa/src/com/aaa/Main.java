71d12fbeac3eaff90634fcfe130a411491c5252a
lotfiinf5 <lotfiinf5@gmail.com>
2023-02-16 10:43:22 -0500

    ttttttttttttttttt

--- aaaaaaaaaaaaaaaa/src/com/aaa/Main.java
+++ aaaaaaaaaaaaaaaa/src/com/aaa/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("===================>3333333333");
 		System.out.println("===================>555555555");
 		System.out.println("===================>222222222222222");
-		System.out.println("===================>2222222222rr22222");
+		System.out.println("ttttttttttttttttttttttttt");
 	}
 
 }
